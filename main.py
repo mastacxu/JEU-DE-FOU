76a19817e73c17ff16f59b5c009071773c32869d
mastacx <thomasmathis213@gmail.com>
2026-06-13 15:07:43 +0200

    debut main

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+import json
+from settings import *
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SCALED)
+pygame.display.set_caption(TITLE)
+clock = pygame.time.Clock()
