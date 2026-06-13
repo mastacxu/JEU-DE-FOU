585cb2d47928ad2c60cbb8191a6fb4d528fb3270
mastacx <thomasmathis213@gmail.com>
2026-06-13 23:09:12 +0200

    ...

---
+++ assets/maps/damien-smyth-rocky-grass-tilesheet-showcase.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="damien-smyth-rocky-grass-tilesheet-showcase" tilewidth="128" tileheight="128" tilecount="192" columns="12">
+ <image source="../../../../Downloads/damien-smyth-rocky-grass-tilesheet-showcase.jpg" width="1536" height="2048"/>
+</tileset>
