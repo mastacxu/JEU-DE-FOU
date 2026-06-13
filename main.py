fe30f1bfdaf47f2f4156b4b7c7f454faf685c5a5
mastacx <thomasmathis213@gmail.com>
2026-06-13 15:10:43 +0200

    main

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ pygame.mixer.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SCALED)
 pygame.display.set_caption(TITLE)
 clock = pygame.time.Clock()
+
+def affichage():
+    print('à remplir')
+
+def jeu():
+    print('à remplir')
+
